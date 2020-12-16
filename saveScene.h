dbb22abec395b8c16e6064d97a84bdfa404bddc4
seungJii <45627002+seungJii@users.noreply.github.com>
2020-12-16 17:40:54 +0900

    Add files via upload

---
+++ saveScene.h
@@ -0,0 +1,24 @@
+typedef struct saveScene{
+
+    TextBlock* TB_Array[MAX_TEXTBLOCK_SIZE];
+    TextBlock* scoreText;
+    TextBlock* Id;
+
+    int TB_cnt;
+    int select;
+
+    int termflags;
+    struct termios	original_mode;
+}SaveScene;
+
+
+int makeSaveScene(SaveScene** sas);
+int initSaveScene(SaveScene** sas);
+int releaseSaveScene(SaveScene** sas);
+int updateSaveScene(SaveScene** sas);
+int renderSaveScene(SaveScene** sas);
+
+int addTextBlockSaveScene(SaveScene** sas,TextBlock* tb);
+int delTextBlockSaveScene(SaveScene** sas);
+
+
